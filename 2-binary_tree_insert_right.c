08ce1bf641eb20462048cf34368286d3b3d94cb1
Mawuli Agropah <wizzyagrops2016@gmail.com>
2023-11-02 23:41:47 +0000

    done with task 2

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -6,7 +6,7 @@
  * @parent: A pointer to the node to insert the right-child in.
  * @value: The value to store in the new node.
  *
- * Return:  must return a pointer to the created node, NULL on failure 
+ * Return:  must return a pointer to the created node, NULL on failure
  * or if parent is NULL.
  */
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
