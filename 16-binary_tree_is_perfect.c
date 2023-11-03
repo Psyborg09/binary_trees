5904a38741abeeb7fd2ef3b68c7381521f8de0f6
Mawuli Agropah <wizzyagrops2016@gmail.com>
2023-11-03 01:41:03 +0000

    done debugging taks 16

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -1,5 +1,10 @@
 #include "binary_trees.h"
 
+int is_perfect_recursive(const binary_tree_t *tree,
+                size_t leaf_depth, size_t level);
+const binary_tree_t *get_leaf(const binary_tree_t *tree);
+size_t depth(const binary_tree_t *tree);
+unsigned char is_leaf(const binary_tree_t *node);
 
 /**
  * binary_tree_is_perfect - Checks if a binary tree is perfect.
